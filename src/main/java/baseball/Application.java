3e1cbcf8d0897098b88797692998dfe74b26bc7d
changjin.park <pcjin118@gmail.com>
2021-10-05 01:41:55 +0900

    feat: 게임을 시작한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import baseball.domain.BaseBallGame;
+
+import static baseball.constant.GameConstant.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
+        BaseBallGame baseBallGame = new BaseBallGame();
+        baseBallGame.playGame();
+
     }
 }
---
+++ src/main/java/baseball/domain/BaseBallGame.java
@@ -0,0 +1,25 @@
+package baseball.domain;
+
+import java.util.List;
+
+import static baseball.constant.GameConstant.*;
+import static baseball.domain.calculate.Calculator.calculateBall;
+import static baseball.domain.calculate.Calculator.calculateStrike;
+import static baseball.domain.computer.NumberGenerator.getComputerNumber;
+import static baseball.domain.user.InputNumber.getUserNumber;
+import static baseball.domain.user.InputNumber.selectGameStatus;
+import static baseball.view.PrintGameResult.printGameResult;
+
+
+public class BaseBallGame {
+    public int playGame() {
+        List<Integer> computerNumber = getComputerNumber();//컴퓨터 숫자 생성
+        int strikeCount = NUMBER_RANGE_ZERO;
+        while (strikeCount != GAME_END) {
+            System.out.print(INPUT_USER_BALL_MESSAGE);
+            List<Integer> userNumber = getUserNumber();//user 숫자 입력
+            //strikeCount = judgeGame(computerNumber, userNumber);
+        }
+        return -1;//endGame();
+    }
+}
