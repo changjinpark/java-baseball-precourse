e891b68eee1d9b20fc95b953337654fbd41bc87e
changjin.park <pcjin118@gmail.com>
2021-10-05 01:36:22 +0900

    feat: 사용자가 입력한 값 검증

---
+++ src/main/java/baseball/domain/user/InputValidation.java
@@ -0,0 +1,39 @@
+package baseball.domain.user;
+
+import java.util.*;
+
+import static baseball.constant.GameConstant.*;
+import static baseball.domain.user.InputNumber.selectGameStatus;
+
+public class InputValidation {
+
+    public static void validateNumberLength(String inputNumber) {
+        if(inputNumber.length() != NUMBER_LENGTH) {
+            System.out.println(INPUT_LENGTH_ERROR_MESSAGE);
+        }
+    }
+
+    public static void validateIsOnlyDigit(String inputNumber) {
+        try {
+            Double.parseDouble(inputNumber);
+        } catch(NumberFormatException e) {
+            System.out.println(INPUT_NOT_DIGIT_ERROR_MESSAGE);
+        }
+    }
+
+    public static void validateZero(String inputNumber) {
+        if(inputNumber.contains("0")) {
+            System.out.println(INPUT_ZERO_ERROR_MESSAGE);
+        }
+    }
+
+    public static void validateNumberDuplicate(String inputNumber) {
+        Set<Character> set = new HashSet<>();
+        for(char x : inputNumber.toCharArray()){
+            set.add(x);
+        }
+        if(set.size() != 3) {
+            System.out.println(INPUT_DUPLICATE_ERROR_MESSAGE);
+        }
+    }
+}
